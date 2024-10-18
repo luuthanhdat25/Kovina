d44febaf2c0c2006036b2f4ce867d11672924e70
Lê Đức Mạnh <116614035+marlymarine131@users.noreply.github.com>
2024-10-18 14:29:39 +0700

    done coding logic change language

---
+++ Assets/Scritps/UI/LangueManager/UIDropdownHandler.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIDropdownHandler : MonoBehaviour
+{
+    public string[] keys;
+    private TMP_Dropdown dropdown;
+    private bool isUpdating = false; 
+
+    void Awake()
+    {
+        dropdown = GetComponent<TMP_Dropdown>();
+        if (dropdown == null)
+        {
+            Debug.LogError("TMP_Dropdown component not found!");
+        }
+    }
+
+    void Start()
+    {
+        LocalizationManager.Instance.OnLanguageChanged += UpdateOptions;
+        UpdateOptions();
+    }
+
+    public void UpdateOptions()
+    {
+        if (isUpdating) return;
+        isUpdating = true;
+
+        if (dropdown != null && keys != null && keys.Length > 0)
+        {
+            // Store the current selection index
+            int currentIndex = dropdown.value;
+
+            dropdown.ClearOptions();
+            var localizedOptions = new List<string>();
+
+            foreach (var key in keys)
+            {
+                string localizedText = LocalizationManager.Instance.GetLocalizedText(key);
+                localizedOptions.Add(localizedText);
+            }
+
+            dropdown.AddOptions(localizedOptions);
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(LocalizationManager.Instance.GetCurrentLanguage() + "_Display"))
+                {
+                    dropdown.value = i;
+                    break;
+                }
+            }
+            dropdown.RefreshShownValue();
+        }
+
+        isUpdating = false; 
+    }
+
+    void OnDestroy()
+    {
+        LocalizationManager.Instance.OnLanguageChanged -= UpdateOptions; // Unsubscribe to prevent memory leaks
+    }
+}
--- Assets/Scritps/UI/SettingController.cs
+++ Assets/Scritps/UI/SettingController.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio; // If you're using AudioMixer for volume control
 
 public class SettingController : MonoBehaviour
 {
+    public AudioMixer audioMixer; 
+
     public void ChangeVolume(float volume)
     {
-        Debug.Log(volume);
+        Debug.Log($"Volume set to: {volume}");
+        
+        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20); // Convert linear to decibel
     }
-    public void ChangeLangue(int index)
+
+    public void ChangeLanguage(int index)
     {
         switch (index)
         {
-            case 0: Debug.Log("Changed to VietNamese");break;
-            case 1: Debug.Log("Changed to Korea"); break;
-            case 2: Debug.Log("Changed to English"); break;
+            case 0:
+                Debug.Log("Changed to Vietnamese");
+                LocalizationManager.Instance.SetLanguage("English");
+                break;
+            case 1:
+                Debug.Log("Changed to Korean");
+                LocalizationManager.Instance.SetLanguage("Vietnamese");
+                break;
+            case 2:
+                Debug.Log("Changed to English");
+                LocalizationManager.Instance.SetLanguage("Korean");
+                break;
+            default:
+                Debug.LogWarning("Language index not recognized.");
+                return; 
         }
     }
-
 }
